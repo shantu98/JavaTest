6896f6b08b6bf55c669f6bf01a40e5f970f40b43
shantu98 <94452752+shantu98@users.noreply.github.com>
2021-12-03 20:33:18 +0530

    Update AvoidThrowingGenericException.java

--- src/main/java/AvoidThrowingGenericException.java
+++ src/main/java/AvoidThrowingGenericException.java
@@ -18,4 +18,18 @@ public class AvoidThrowingGenericException {
     public void M5()throws IOException, InvocationTargetException, SQLException{
         logger.info("Throwing IOException, InvocationTargetException, SQLException");
     }
+    try{
+      Connection con = DriverManager.getConnection(mysqlUrl, "root", "password");
+      Statement stmt = con.createStatement(); {
+         try(ResultSet rs = stmt.executeQuery("select * from MyPlayers");){
+            //Retrieving the data
+            while(rs.next()) {
+            
+            }
+         } catch (SQLException e) {
+            e.printStackTrace();
+         }
+      }} catch (SQLException e) {
+            e.printStackTrace();
+      }
 }
